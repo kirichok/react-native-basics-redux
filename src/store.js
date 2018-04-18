2a53b6184831b82f4c5a65843d3ad5b6f0ddacae
evgeniy <evgeniy.kirichok@gmail.com>
2018-04-18 16:24:50 +0300

    initial

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+
+let _reducers;
+
+function defineReducers(reducers) {
+    _reducers = combineReducers(reducers);
+}
+
+export default () => {
+    return createStore(_reducers, applyMiddleware(thunk, promiseMiddleware()));
+}
